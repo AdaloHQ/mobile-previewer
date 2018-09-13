3c0ae99265956a03e861b8599190598837bedce2
Jeremy Blalock <jeremyjblalock@gmail.com>
2018-09-13 01:05:44 -0700

    Adds new dependencies

--- android/app/src/main/java/com/protonpreviewer/MainApplication.java
+++ android/app/src/main/java/com/protonpreviewer/MainApplication.java
@@ -3,6 +3,8 @@ package com.protonpreviewer;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import cl.json.RNSharePackage;
 import com.horcrux.svg.SvgPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.reactlibrary.RNIcloudFilePickerPackage;
@@ -28,6 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
+            new RNSharePackage(),
             new SvgPackage(),
             new ImagePickerPackage(),
             new RNIcloudFilePickerPackage(),
