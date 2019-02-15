079d755617fef8934daa39b35462a12a277e9b52
Jeremy Blalock <jeremyjblalock@gmail.com>
2019-02-14 16:16:36 -0800

    Adds firebase config

--- android/app/src/main/java/com/protonpreviewer/MainApplication.java
+++ android/app/src/main/java/com/protonpreviewer/MainApplication.java
@@ -3,6 +3,10 @@ package com.protonpreviewer;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage
+
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
@@ -30,6 +34,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
+            new RNFirebaseNotificationsPackage()
             new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
             new SvgPackage(),
