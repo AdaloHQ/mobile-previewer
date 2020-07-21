d82722fd7e2d5bcea14cae47f501058f571e7f74
max <maxwellrocco79@gmail.com>
2020-07-21 16:54:55 -0400

    changes to make android work

--- libraries.js
+++ libraries.js
@@ -1,5 +1,7 @@
 import * as lib1 from '@protonapp/material-components'
+import * as lib2 from 'image-carousel'
 
 export default {
   '@protonapp/material-components': lib1,
+  'image-carousel': lib2,
 }
