6dfbb779ad425ae22d14d4c77acbb66cc0f4641f
Toby Sullivan <toby.sullivan@adalo.com>
2022-06-07 11:43:02 -0700

    Initialize tracing

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
+import { initTracing } from '@protonapp/proton-runner'
 import { AppRegistry } from 'react-native'
+
 import App from './App'
 import { name as appName } from './app.json'
+
+// Initialize tracing with null DSN so that no transactions are captured or sent.
+initTracing(null)
 
 AppRegistry.registerComponent(appName, () => App)
